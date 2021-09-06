52606d820570b37f9d253c88c5d86ab81608d654
anmolahuja-dev <anmolahuja359@gmail.com>
2021-09-07 00:30:55 +0530

    [Major Update]- Phone Number Functionality Running

--- src/components/PhoneAuth/PhoneAuth.js
+++ src/components/PhoneAuth/PhoneAuth.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import firebase from '../firebase';
 import { getAuth,RecaptchaVerifier,signInWithPhoneNumber } from "firebase/auth";
+import '../../css/otpInput.css'
 
 export default class PhoneAuth extends Component {
     constructor(props){
@@ -24,27 +25,35 @@ export default class PhoneAuth extends Component {
     }
 
     onSignInSubmit = (e) => {
-        console.log("onsign in clicked");
         e.preventDefault();
-        this.verifyBtnRef.current.classList.add('d-none');
-        this.otpdivRef.current.classList.remove('d-none');
+        if(this.state.phone==='' || this.state.phone.length<10){
+            this.phoneRef.current.classList.add("is-invalid");
+        }
+        else {
 
-        this.setUpRecaptcha();
-        
-        const phoneNumber = "+91"+this.state.phone;
-        console.log(phoneNumber);
-        const appVerifier = window.recaptchaVerifier;
+            if(this.phoneRef.current.classList.contains('is-invalid')){
+                this.phoneRef.current.classList.remove("is-invalid");
+            }
+            this.verifyBtnRef.current.classList.add('d-none');
+            this.otpdivRef.current.classList.remove('d-none');
 
-        const auth = getAuth();
-        signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-            .then((confirmationResult) => {
-                
-                window.confirmationResult = confirmationResult;
-                console.log('otp sent');
+            this.setUpRecaptcha();
+            
+            const phoneNumber = "+91"+this.state.phone;
+            
+            const appVerifier = window.recaptchaVerifier;
 
-            }).catch((error) => {
-                console.log("otp send error");
-            });
+            const auth = getAuth();
+            signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+                .then((confirmationResult) => {
+                    
+                    window.confirmationResult = confirmationResult;
+                    console.log('otp sent');
+
+                }).catch((error) => {
+                    console.log("otp send error");
+                });
+        }
     }
 
     setUpRecaptcha = () => {
@@ -60,16 +69,32 @@ export default class PhoneAuth extends Component {
 
     onSubmitOtp = (e) => {
         e.preventDefault();
-        console.log("clicked");
-        const code = this.state.otp;
-        console.log(code);
-        window.confirmationResult.confirm(code).then((result) => {
-            
-            const user = result.user;
-            alert("Success")
-        }).catch((error) => {
-            console.log("Fail");
-        });
+        
+        if(this.state.otp || this.state.otp.length<6 ){
+            this.otpRef.classList.add('is-invalid');
+        }
+        else {
+            if(this.otpRef.classList.contains('is-invalid')){
+                this.otpRef.classList.remove('is-invalid')
+            }
+
+            const code = this.state.otp;
+            window.confirmationResult.confirm(code).then((result) => {
+                
+                const user = result.user;
+                this.phoneRef.current.classList.add('is-valid');
+                this.otpdivRef.current.classList.add('d-none');
+
+            }).catch((error) => {
+                console.log("Fail");
+                this.phoneRef.current.classList.add('is-invalid');
+                this.otpdivRef.current.classList.add('d-none');
+                this.verifyBtnRef.current.classList.remove('d-none');
+                this.setState({
+                    otp:''
+                });
+            });
+        }
     }
 
     render() {
@@ -77,44 +102,47 @@ export default class PhoneAuth extends Component {
             <div>
                 <form onSubmit={this.onSignInSubmit} >
                     <div className="mb-1 ms-4 mt-2">
-                        <label htmlFor="phoneNumber" className="form-label">Phone number</label>
+                        <label htmlFor="phoneNumber" className="form-label d-block">Phone number</label>
                         <input 
-                            type="text" 
-                            className="form-control form__field"
+                            type="number" 
+                            className="form-control form__field phoneNumber d-inline "
                             id="phoneNumber"
                             value={this.state.phone}
                             onChange={this.onChangeHandler}
                             name="phone"
                             ref={this.phoneRef}
+                            maxLength="10"
                         />
+                        
                         <button 
-                            className="btn btn-sm btn-outline-dark mt-1 verifyBtn"
+                            className="btn btn-sm btn-outline-dark ms-3 mt-1 verifyBtn "
                             type="submit"
                             ref={this.verifyBtnRef}
                         >
                             verify
                         </button>
-                        <div id="recaptcha-container"></div>
                         <div className="invalid-feedback " ref={this.phoneError}>
-                
+                            Please Enter a valid phone number
                         </div>
+                        <div id="recaptcha-container"></div>
                     </div>
                 </form>
                 <form onSubmit={this.onSubmitOtp}>    
-                    <div className="mb-1 ms-4 mt-2 d-none " ref={this.otpdivRef}>
-                        
+                    <div className="mb-1 ms-4 mt-2 d-none" ref={this.otpdivRef}>
+                        <label htmlFor="otp" className="mt-2">Enter OTP :  </label>
                         <input 
                             type="text" 
-                            className="form-control form__field"
+                            className="form-control form__field partitionedOTP d-inline"
                             id="otp"
                             value={this.state.otp}
                             onChange={this.onChangeHandler}
                             name="otp"
                             ref={this.otpRef}
+                            maxLength="6"
                         />
                         <button
                             type="submit"
-                            className="btn btn-sm btn-outline-dark mt-1 verifyBtn"
+                            className="btn btn-sm btn-outline-dark mt-1 ms-3 verifyBtn d-inline"
                         >submit</button>
                         <div className="invalid-feedback " ref={this.otpError}>
                         </div>
